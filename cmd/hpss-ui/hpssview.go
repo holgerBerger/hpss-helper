4c3498de15753b74335bb2bc708a5c976106eebd
Holger Berger <holger.berger@googlemail.com>
2017-08-22 17:23:25 +0200

    restructured hpsss view completed navigation in hpssview

--- cmd/hpss-ui/hpssview.go
+++ cmd/hpss-ui/hpssview.go
@@ -10,19 +10,23 @@ import (
 	"strings"
 )
 
-var (
-	hpsstoplevel         bool = true
-	hpsscurrentarchive   string
-	currenthpssselection int
-	currenthpssorigin    int
-	hpssviewprefix       string
-	hpsslines            []string // file as lines
-	hpssfolderlines      []string // contents of current folder
-)
+type hpssT struct {
+	toplevel       bool     // true if on top level = view of archive names
+	currentarchive string   // name of current archive
+	cursorline     int      // line with cursorline
+	origin         int      // top line of displayed
+	prefix         string   // commom prefix part of path which is not displayed
+	lines          []string // lines of file
+	folderlines    []string // content of current folder to display
+}
 
+var hpss hpssT
+
+// dar the view frame
 func fillHpss(v *gocui.View) {
-	if hpsstoplevel {
+	if hpss.toplevel { // list of archives
 		v.Title = "HPSS archive - available archives"
+
 		archives, err := filepath.Glob(os.ExpandEnv(config.General.Cachedir) + "/*.full")
 		if err != nil {
 			panic(err)
@@ -30,7 +34,7 @@ func fillHpss(v *gocui.View) {
 			currentline := 0
 
 			for _, file := range archives {
-				if currenthpssselection == currentline {
+				if hpss.cursorline == currentline {
 					fmt.Fprint(v, "\x1b[0;30;47m>")
 				} else {
 					fmt.Fprint(v, " ")
@@ -39,17 +43,30 @@ func fillHpss(v *gocui.View) {
 				currentline++
 			}
 		}
-	} else {
-		v.Title = "HPSS archive -" + hpsscurrentarchive
+	} else { // not toplevel, but in archive
+		v.Title = "HPSS archive " + hpss.currentarchive + " " + hpss.prefix
 
+		// ..
 		currentline := 0
-		for _, i := range hpssfolderlines {
-			if currenthpssselection == currentline {
+		if hpss.cursorline == currentline {
+			fmt.Fprint(v, "\x1b[0;30;47m>")
+		} else {
+			fmt.Fprint(v, " ")
+		}
+		fmt.Fprintln(v, "\x1b[0;32m..")
+		currentline++
+		// entries
+		for _, i := range hpss.folderlines {
+			if hpss.cursorline == currentline {
 				fmt.Fprint(v, "\x1b[0;30;47m>")
 			} else {
 				fmt.Fprint(v, " ")
 			}
-			fmt.Fprintln(hpssview, i)
+			if strings.Contains(i, "|") {
+				fmt.Fprintln(hpssview, "\x1b[0;30;47m"+i)
+			} else {
+				fmt.Fprintln(hpssview, "\x1b[0;32m"+i)
+			}
 			currentline++
 		}
 
@@ -58,13 +75,13 @@ func fillHpss(v *gocui.View) {
 
 // move cursor up and scroll if necessary
 func hpssCursorUp(g *gocui.Gui, v *gocui.View) error {
-	if currenthpssselection > 0 {
-		currenthpssselection--
+	if hpss.cursorline > 0 {
+		hpss.cursorline--
 	}
 	//_, y := v.Size()
-	if currenthpssselection < currenthpssorigin {
-		currenthpssorigin--
-		v.SetOrigin(0, currenthpssorigin)
+	if hpss.cursorline < hpss.origin {
+		hpss.origin--
+		v.SetOrigin(0, hpss.origin)
 	}
 	v.Clear()
 	v.Rewind()
@@ -74,16 +91,16 @@ func hpssCursorUp(g *gocui.Gui, v *gocui.View) error {
 
 // move cursur down and scroll if necessary
 func hpssCursorDown(g *gocui.Gui, v *gocui.View) error {
-	currenthpssselection++
-	newline, err := v.Line(currenthpssselection)
+	hpss.cursorline++
+	newline, err := v.Line(hpss.cursorline)
 	if err != nil || len(newline) == 0 {
-		currenthpssselection--
+		hpss.cursorline--
 	}
 
 	_, y := v.Size()
-	if currenthpssselection >= currenthpssorigin+y {
-		currenthpssorigin++
-		v.SetOrigin(0, currenthpssorigin)
+	if hpss.cursorline >= hpss.origin+y {
+		hpss.cursorline++
+		v.SetOrigin(0, hpss.origin)
 	}
 	v.Clear()
 	v.Rewind()
@@ -91,30 +108,32 @@ func hpssCursorDown(g *gocui.Gui, v *gocui.View) error {
 	return nil
 }
 
+// read archive cache file into hpss.lines and fill current folder data
 func readArchiveCache() {
-	file, err := ioutil.ReadFile(config.General.Cachedir + "/" + hpsscurrentarchive + ".full")
+	file, err := ioutil.ReadFile(config.General.Cachedir + "/" + hpss.currentarchive + ".full")
 	if err != nil {
 		fmt.Fprintln(logview, "could not read archive cache!")
 		return
 	}
-	hpsslines = strings.Split(string(file), "\n")
-	hpssviewprefix = ""
+	hpss.lines = strings.Split(string(file), "\n")
+	hpss.prefix = ""
 	getFolderContens()
 }
 
+// filter current folder from hpss.lines by showing lines with common prefix
 func getFolderContens() {
 	shown := make(map[string]bool)
-  hpssfolderlines=make([]string, 0,0 )
+	hpss.folderlines = make([]string, 0, 0)
 
-	for _, line := range hpsslines {
-		if (len(hpssviewprefix)<=len(line)) && (line[:len(hpssviewprefix)] == hpssviewprefix) {
-			dir := line[len(hpssviewprefix):] // prefix removed
+	for _, line := range hpss.lines {
+		if (len(hpss.prefix) <= len(line)) && (line[:len(hpss.prefix)] == hpss.prefix) {
+			dir := line[len(hpss.prefix):] // prefix removed
 			entry := strings.Split(dir, "/")[0]
 			if _, ok := shown[entry]; ok {
 				// already displayed, skip
 			} else {
 				shown[entry] = true
-				hpssfolderlines=append(hpssfolderlines, entry)
+				hpss.folderlines = append(hpss.folderlines, entry)
 			}
 		}
 	}
@@ -122,49 +141,47 @@ func getFolderContens() {
 
 // chamge mode if on top level or change directory
 func hpssEnter(g *gocui.Gui, v *gocui.View) error {
-	if hpsstoplevel {
-		hpsstoplevel = false
-		line, _ := v.Line(currenthpssselection)
-		hpsscurrentarchive = line[1:]
-		currenthpssselection = 0
+	if hpss.toplevel {
+		hpss.toplevel = false
+		line, _ := v.Line(hpss.cursorline)
+		hpss.currentarchive = line[1:]
+		hpss.cursorline = 0
 		readArchiveCache()
 		v.Clear()
 		v.Rewind()
 		fillHpss(v)
 	} else {
-		line, _ := v.Line(currenthpssselection)
-		fmt.Fprintln(logview, "selected "+line )
-		hpssviewprefix = hpssviewprefix + line[1:]+"/"
-		fmt.Fprintln(logview, hpssviewprefix)
-		currenthpssselection = 0
-		currenthpssorigin = 0
+		line, _ := v.Line(hpss.cursorline)
+		fmt.Fprintln(logview, "selected "+line)
+		if line[1:] == ".." { // going up
+			fmt.Fprintln(logview, "to split: "+hpss.prefix)
+			splitted := strings.Split(hpss.prefix, "/")
+			fmt.Fprintln(logview, splitted, len(splitted))
+			if len(splitted) <= 1 { // top level
+				hpss.toplevel = true
+				hpss.prefix = ""
+			} else if len(splitted) <= 2 { // one below toplevel
+				hpss.prefix = ""
+			} else { // deeper levels
+				hpss.prefix = strings.Join(splitted[:len(splitted)-2], "/") + "/"
+			}
+		} else { // going down
+			hpss.prefix = hpss.prefix + line[1:] + "/"
+		}
+		fmt.Fprintln(logview, "prefix: "+hpss.prefix)
+		hpss.cursorline = 0
+		hpss.origin = 0
 		getFolderContens()
 		v.Clear()
 		v.Rewind()
 		fillHpss(v)
-
-		/*
-			line, _ := v.Line(currentfsselection)
-			newdir := line[2:]
-			err := os.Chdir(newdir)
-			if err != nil {
-				fmt.Fprintln(logview, err)
-				return nil
-			}
-			currentdir, _ = os.Getwd()
-			fsselection = make(map[int]bool)
-			currentfsselection = 0
-			currentfiles = make([]os.FileInfo, 0, 0)
-			v.Clear()
-			v.Rewind()
-			fillFs(v)
-		*/
 	}
 	return nil
 }
 
 // create key bindings for fs viewer
 func hpssviewkeybindings(g *gocui.Gui) {
+	hpss.toplevel = true
 	// cursor up
 	if err := g.SetKeybinding("hpss", gocui.KeyArrowUp, gocui.ModNone, hpssCursorUp); err != nil {
 		log.Panicln(err)
